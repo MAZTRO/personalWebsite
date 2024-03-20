1c97a6d4e72f9eea9cefc90afbea19fc764f4578
MAZTRO <design.mallet@gmail.com>
2024-03-20 15:38:50 -0500

    Adding more components

---
+++ frontend/src/components/experience/data.js
@@ -0,0 +1,22 @@
+// Array de datos de experiencia de trabajos anteriores
+
+export const data = [
+  {
+    title: 'Trabaje aqui',
+    subtitle: 'Frontend developer',
+    dates: new Date('Mar 21 2021').toLocaleDateString(),
+    description: 'lorem sfdg adfgadfg adfg adfhga df hafgadfgadfgadf gadfgadf g',
+  },
+  {
+    title: 'Trabaje aqui',
+    subtitle: 'Frontend developer',
+    dates: new Date('Mar 21 2021').toLocaleDateString(),
+    description: 'lorem sfdg adfgadfg adfg adfhga df hafgadfgadfgadf gadfgadf g',
+  },
+  {
+    title: 'Trabaje aqui',
+    subtitle: 'Frontend developer',
+    dates: new Date('Mar 21 2021').toLocaleDateString(),
+    description: 'lorem sfdg adfgadfg adfg adfhga df hafgadfgadfgadf gadfgadf g',
+  },
+]
