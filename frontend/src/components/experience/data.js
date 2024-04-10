18bb106fa216e94aff17f64cd6ad64e162a21cea
MAZTRO <design.mallet@gmail.com>
2024-04-09 20:23:53 -0500

    Adding more data and fix some css styles

--- frontend/src/components/experience/data.js
+++ frontend/src/components/experience/data.js
@@ -2,21 +2,17 @@
 
 export const data = [
   {
-    title: 'Trabaje aqui',
-    subtitle: 'Frontend developer',
-    dates: new Date('Mar 21 2021').toLocaleDateString(),
-    description: 'lorem sfdg adfgadfg adfg adfhga df hafgadfgadfgadf gadfgadf g',
+    fDate: 'Oct 01 2020',
+    lDate: 'Mar 31 2024',
+    title: 'Full Stack Engineer',
+    subtitle: 'Ozel producciones S.A.S',
+    description: 'I designed and optimized applications handling high user concurrency, including chat functionalities, interactive stores, and virtual fairs. I spearheaded improvements that reduced API request processing time from 3 minutes to 10 seconds by implementing asynchronous backend calls for email delivery and comprehensive metric gathering.',
   },
   {
-    title: 'Trabaje aqui',
-    subtitle: 'Frontend developer',
-    dates: new Date('Mar 21 2021').toLocaleDateString(),
-    description: 'lorem sfdg adfgadfg adfg adfhga df hafgadfgadfgadf gadfgadf g',
-  },
-  {
-    title: 'Trabaje aqui',
-    subtitle: 'Frontend developer',
-    dates: new Date('Mar 21 2021').toLocaleDateString(),
-    description: 'lorem sfdg adfgadfg adfg adfhga df hafgadfgadfgadf gadfgadf g',
-  },
+    fDate: 'Mar 01 2020',
+    lDate: 'Jun 30 2020',
+    title: 'Full Stack Engineer',
+    subtitle: 'SIM-TAC',
+    description: 'The entire interface was made in Vanilla JavaScript and Vanilla CSS, which allowed me to solidify my knowledge in the core of the languages and thus be able to learn the JS libraries more easily. Also the implementation of calls to a third-party API with Python to optimize the loading time of the graphs',
+  }
 ]
