8952ae2ee64c5b6c9cbf7416ef798420e8a9e8e9
MAZTRO <design.mallet@gmail.com>
2024-04-22 19:46:05 -0500

    Adding project to jobs popup

---
+++ frontend/src/components/experience/jobs/jobsData.js
@@ -0,0 +1,14 @@
+export const JOBS = [
+  {
+    name: 'Sorting visualizer',
+    description: 'This project is a sorting visualizer web application built with React, Typescript, and Tailwind CSS. It allows users to explore different sorting algorithms and visualize their execution in real-time.',
+    image: 'img/projects/sortingVisualizer.png',
+    link: 'https://sorting-v.jonatam.com/',
+    code: 'https://github.com/MAZTRO/sorting_visualizer',
+    tags: [
+      'React',
+      'Tailwind',
+      'TypeScript',
+    ]
+  }
+]
